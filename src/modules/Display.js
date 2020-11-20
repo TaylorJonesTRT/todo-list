d442e206f312b623a1bf93abecb4490e73ef4783
Taylor Jones <taylor@taylorwjones.com>
2020-11-19 19:05:19 -0600

    Progress on the display controller

--- src/modules/Display.js
+++ src/modules/Display.js
@@ -13,12 +13,37 @@ const displayController = () => {
     logoDiv.classList.add("logo");
     let logoImage = document.createElement("i");
     logoImage.classList.add("logo-icon", "fas", "fa-th-list", "fa-2x");
+    let logoHR = document.createElement("hr");
+    
+    let sidebarContainerDiv = document.createElement("div");
+    sidebarContainerDiv.id = "sidebar-container";
+    let favoritesUL = document.createElement("ul");
+    favoritesUL.classList.add("favorites");
+    // let favoritesLI = document.createElement("li");
+    // favoritesLI.classList.add("project");
+    let sidebarHR = document.createElement("hr");
+    sidebarHR.classList.add("sidebar-container-hr");
 
     // Appending Elements
     appDiv.appendChild(containerDiv);
     containerDiv.appendChild(sidebarDiv);
     sidebarDiv.appendChild(logoDiv);
     logoDiv.appendChild(logoImage);
+    logoDiv.appendChild(logoHR);
+    sidebarDiv.appendChild(sidebarContainerDiv);
+    sidebarContainerDiv.appendChild(favoritesUL);
+    sidebarContainerDiv.appendChild(sidebarHR);
+
+    // delete this, just to make sure appending favorites menu works
+    for (let i = 0; i < 5; i++) {
+        let favoritesLI = document.createElement("li");
+        if (i === 1) {
+            favoritesLI.classList.add("project-active");
+        } else {
+            favoritesLI.classList.add("project");
+        }
+        favoritesUL.appendChild(favoritesLI).innerText = i;
+    }
 }
 
 export { displayController }
