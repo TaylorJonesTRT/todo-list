660e0767fdaac688be1ca51d2756738bc597ee8e
Taylor Jones <taylor@taylorwjones.com>
2020-11-22 17:42:35 -0600

    Worked on logic as well... oops

--- src/modules/Projects.js
+++ src/modules/Projects.js
@@ -4,13 +4,25 @@ import DisplayController from "./Display";
 import ProjectModel from "./models/project.model";
 
 class Projects {
-    constructor() {    
+    constructor() {
         this.projects = JSON.parse(localStorage.getItem('projects')) || [new ProjectModel('Default')];
+        this.favorites = ["Inbox", "Today", "Soon"];
+        this.inbox = [];
+        this.today = [];
+        this.soon = [];
         this.pageRender();
     }
 
     createProject(title) {
         const newProject = new ProjectModel(title);
+        this.projects.push(newProject);
+        localStorage.setItem('projects', JSON.stringify(this.projects));
+    }
+
+    pageRender() {
+        let render = new DisplayController();
+        render.renderFavorites(this.favorites);
+        // render.renderProjects(this.projets);
     }
 }
 
--- src/modules/models/project.model.js
+++ src/modules/models/project.model.js
@@ -1,8 +1,9 @@
 class ProjectModel {
-    constructor(title, todos = []) {
-        this.id = id;
+    constructor(id = 3, title, todos = []) {
         this.title = title;
         this.todos = todos;
+        this.id = id;
+        this.id++;
     }
 }
 
